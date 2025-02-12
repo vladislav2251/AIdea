1cb7113e130b915a9a5a48a88bc2e67487536c1e
vladislav2251 <vladdidyk1985@gmail.com>
2025-02-12 10:08:21 +0200

    feat: added heading file

---
+++ components/heading.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface IProps {
+    title: string;
+    description: string;
+};
+
+const props = defineProps<IProps>();
+</script>
+
+<template>
+    <h2 class="text-4xl font-bold">
+        {{ props.title }}
+    </h2>
+    <p class="my-4 text-base text-muted-foreground">
+        {{ props.description }}
+    </p>
+</template>
